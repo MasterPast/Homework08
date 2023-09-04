9a6f4ab4decad8576a6f009603f8495efcb93e08
MasterPast <igory733@gmail.com>
2023-09-04 16:43:38 +0300

    cont-3

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 from datetime import date, datetime, timedelta
-from collections import Counter, defaultdict
+# from collections import Counter, defaultdict
 
 
 def get_birthdays_per_week(users):
     #  Реалізуйте тут домашнє завдання
-    #  {"name": "Bill Gates", "birthday": datetime(1955, 10, 28).date()}
-    #  {'Monday': ['Bill', 'Jan'], 'Wednesday': ['Kim']}
 
     dict_births = {}
-    today = datetime(year=2023, month=12, day=26).date()
-    # today = datetime.today().date()
-    # print(today)
+    today = datetime(year=2023, month=12, day=26).date()    #  Встановлення статичної дати замість стогоднішньої
+    # today = datetime.today().date()                       #  Встановлення сьогоднішної дати 
+
     time_delta = timedelta(days=7)
     date_end = today + time_delta
-    # print(date_end)
 
     for dict_user in users:
         for day_match in range(0, time_delta.days):
             dat_delta = timedelta(days=day_match)
             dat_birth = dict_user['birthday']
 
-            if dict_user['birthday'].year == (dict_user['birthday']-dat_delta).year:
+            if dict_user['birthday'].year == (dict_user['birthday']-dat_delta).year:    # Перевірка, чи переходить Дельта із року в рік
                 dat_birth = datetime(
                     year=today.year, month=dat_birth.month, day=dat_birth.day)
             else:
                 dat_birth = datetime(
                     year=today.year+1, month=dat_birth.month, day=dat_birth.day)
 
-            if dat_birth.date() == today + dat_delta:
-                if (today+dat_delta).strftime('%A') == 'Monday':
+            if dat_birth.date() == today + dat_delta:                                   #  Перевіряє пранлежність кожного ДН до відповідного
+                if (today+dat_delta).strftime('%A') == 'Monday':                        #  дня тижня, та формує вихідний список
                     if ('Monday') not in dict_births:
                         dict_births['Monday'] = []
                     dict_births['Monday'].append(dict_user['name'])
@@ -88,14 +85,7 @@ def get_birthdays_per_week(users):
 if __name__ == "__main__":
     users = [
         {"name": "Jan Koum", "birthday": datetime(1976, 9, 1).date()},
-        {"name": "Alice", "birthday": datetime(2023, 9, 5).date()},
-        {"name": "John", "birthday": datetime(2023, 9, 3).date()},
-        {"name": "Doe", "birthday": datetime(2024, 9, 4).date()},
-        {"name": "Maria", "birthday": datetime(1976, 9, 2).date()},
-        {"name": "Sylvia", "birthday": datetime(1976, 9, 1).date()},
-        {"name": "Jessica", "birthday": datetime(1976, 9, 6).date()},
-        {"name": "Pamela", "birthday": datetime(1976, 9, 7).date()},
-        {"name": "Cortny", "birthday": datetime(1990, 9, 8).date()},
+        
     ]
 
     result = get_birthdays_per_week(users)
